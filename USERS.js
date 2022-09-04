2884a20cefc35cbc9558f7e495b7712fc297757d
Byri Saikiran <102993626+saibairi@users.noreply.github.com>
2022-09-04 11:26:26 +0530

    Add files via upload

---
+++ USERS.js
@@ -0,0 +1,25 @@
+const users = [
+    {
+        id : 1,
+        name : 'sai1',
+        email : 'sai1@gmail.com'
+    },
+    {
+        id : 2,
+        name : 'sai2',
+        email : 'sai2@gmail.com'
+    },
+    {
+        id : 3,
+        name : 'sai3',
+        email : 'sai3@gmail.com'
+    },
+    {
+        id : 4,
+        name : 'sai4',
+        email : 'sai4@gmail.com'
+    },
+];
+
+
+module.exports = users;
